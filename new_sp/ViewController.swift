6de4f7ad55df60174f2e3c08a1b696325042b465
priyankakanse123 <priyankakanse93@gmail.com>
2016-02-01 11:27:02 +0530

    Initial Commit

---
+++ new_sp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new_sp
+//
+//  Created by BridgeLabz on 01/02/16.
+//  Copyright © 2016 BridgeLabz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
